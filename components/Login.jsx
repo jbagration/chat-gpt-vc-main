a498b6b0eaed6146c106896f8ea810b50bea3bd2
Yulia Ivanova <113036475+jbagration@users.noreply.github.com>
2023-02-21 18:49:33 +0500

    fix: delete form

--- components/Login.jsx
+++ components/Login.jsx
@@ -12,23 +12,6 @@ function Login() {
 
       <h2>Авторизация</h2>
 
-<form>
-    <p>
-      <input className="login-input" type="email" id="email" name="email" placeholder="Email"></input>
-      <label className="login-label" for="email" id="emailLabel"></label>
-    </p>
-    <p>
-      <input className="login-input " type="password" id="password" name="password" placeholder="Пароль"></input>
-      <label className="login-label" for="password" id="passwordLabel"></label>
-    </p>
-    <p>
-      <input className='login-button' type="submit" value="Продолжить" id="submitBtn" disabled></input>
-    </p>
-  </form>
-
-<h5>Нет аккаунта? <button onClick={() => history.push("/register")}>Зарегистрируйтесь</button></h5>
-
-
       <button
         className="google-button text-black font-bold text-3xl flex justify-center items-center bg-white p-4 gap-3 rounded-lg shadow-lg hover:shadow-none"
         onClick={() => signIn("google")}
